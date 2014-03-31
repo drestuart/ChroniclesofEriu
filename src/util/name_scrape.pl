b94a94c148216703183550ccc00e2b34d53d5d6a
Dan Stuart <dstuart@barracuda.com>
2014-03-31 16:48:22 -0400

    Added place name file

--- src/util/name_scrape.pl
+++ src/util/name_scrape.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 use 5.010;
 
-$text = `cat female`;
+$text = `cat places_raw`;
 
+=pod
 while ($text =~ m/<tr>\n<td><b>(.+?)<\/b><\/td>/g) {
 	$names = $1;
 
@@ -10,3 +11,8 @@ while ($text =~ m/<tr>\n<td><b>(.+?)<\/b><\/td>/g) {
 		say $1;
 	}
 }
+=cut
+
+$text =~ s/,\s+/\n/g;
+$text =~ s/\(.+?\)//g;
+say $text;
