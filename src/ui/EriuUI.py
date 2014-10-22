348ac9a3f5545c192eb8d046c1706d8305a680fd
Dan Stuart <dstuart@barracuda.com>
2014-10-22 16:02:33 -0400

    Moved image fade-in logic into dl's UI class

--- src/ui/EriuUI.py
+++ src/ui/EriuUI.py
@@ -7,7 +7,6 @@ Created on Oct 20, 2014
 from UIClass import UI
 import pygame
 import os.path
-import Const as C
 
 class EriuUI (UI):
     
@@ -15,38 +14,11 @@ class EriuUI (UI):
         super(EriuUI, self).__init__(**kwargs)
         
     def ShowLogo(self):
-        loadTime = 2000. #ms
-        alphaMax = 255
-        alphaSteps = 30
-        fadeInDelay = int(loadTime/alphaSteps)
-        
-        # Load and scale the logo image
-        image = pygame.image.load(os.path.join("data", "img", "logo_kells.png")).convert()
-        imageWidth, imageHeight = image.get_width(), image.get_height()
-        imageRatio = float(imageHeight)/imageWidth
-        
-        windowWidth, windowHeight = self.window.pixelsize
-        
-        newWidth = int(windowWidth*.9)
-        newHeight = int(newWidth*imageRatio)
-        
-        image = pygame.transform.scale(image, (newWidth, newHeight))
-        
-        # Logo postioning -- centered
-        logox, logoy = ((windowWidth - newWidth)/2, (windowHeight - newHeight)/2)
-        
-        # Fade in logo
-        for i in range(alphaSteps):
-            self.window.surface.fill((0,0,0))
+        # Show logo image
+        logoPath = os.path.join("data", "img", "logo_kells.png")
+        self.fadeInImage(logoPath, 2000, 30)
             
-            alpha = i*alphaMax/alphaSteps
-            image.set_alpha(alpha)
-            
-            self.window.surface.blit(image, (logox, logoy))
-            self.drawWindow()
-            pygame.time.delay(fadeInDelay)
-            
-        # Clear
+        # Wait a sec, then clear screen
         pygame.time.delay(1000)
         self.window.surface.fill((0,0,0))
         self.drawWindow()
