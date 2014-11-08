fb5d1de5e250b68bef473f6940258bdd4b006124
Dan Stuart <dstuart@barracuda.com>
2014-11-08 00:33:39 -0500

    Updated eriu.py

--- eriu.py
+++ eriu.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python2.7-32
+
 '''
 Created on Mar 10, 2013
 
 @author: dstu
 '''
 
-import Game as G
+import site
 import os
 
+site.addsitedir(os.getcwd())
+
+import Const as C
+from EriuGame import game
+
+fontsize = 14
+font = u'FreeMono.otf'
 
 def main():
     x = 100
     y = 50
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+
+    print '-=' + C.TITLE + '=-'
     
-    print '-=Tales of Eriu=-'
+    game.initialize(fontsize = fontsize, font = font, debug = False)
     
-    game = G.Game(debug = False)
     game.play()
-
-
 
 if __name__ == '__main__':
     main()
