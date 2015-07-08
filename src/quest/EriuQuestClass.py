daa1df32b863d1fddc7c304eb92f95b7345a7e45
Dan Stuart <dstuart@barracuda.com>
2015-07-08 17:02:44 -0400

    dl quest status and convo stuff, added some convos for the test quest

--- src/quest/EriuQuestClass.py
+++ src/quest/EriuQuestClass.py
@@ -15,6 +15,7 @@ class EriuQuest(Quest):
 class testQuest(EriuQuest):
     
     def __init__(self):
+        super(testQuest, self).__init__()
         self.buildRequirements()
         self.placeQuestItems()
     
@@ -28,20 +29,33 @@ class testQuest(EriuQuest):
         currentLevel = EriuGame.game.ui.getCurrentLevel()
         currentLevel.placeItemAtRandom(mg)
         
-    def getConversation(self):
-        firstNode = C.ConversationNode("We don't have much time. I need you to get that Mystic MacGuffin for me.")
-        secondNode = C.ConversationNode("It's over there. Can you do it?")
-        
-        nodes = [firstNode, secondNode]
-        
-        firstNode.createOption("Go on...", secondNode)
-        secondNode.createOption("Sure, no problem", None, self.startQuest)
-        secondNode.createOption("Not happening, buddy")
-        
-        self.conversation = C.ConversationTree(nodes)
-        return self.conversation
+    def getStartConversation(self):
+        if not self.startConversation:
+            firstNode = C.ConversationNode("We don't have much time. I need you to get that Mystic MacGuffin for me.")
+            secondNode = C.ConversationNode("It's over there. Can you do it?")
+            
+            nodes = [firstNode, secondNode]
+            
+            firstNode.createOption("Go on...", secondNode)
+            secondNode.createOption("Sure, no problem", None, self.startQuest)
+            secondNode.createOption("Not happening, buddy")
+            
+            self.startConversation = C.ConversationTree(nodes)
+        return self.startConversation
 
-
+    def getProgressConversation(self):
+        if not self.progressConversation:
+            node = C.ConversationNode("Have you found that MacGuffin yet? They say it's Mystical as all get-out!")
+            node.createOption("OK")
+            self.progressConversation = C.ConversationTree([node])
+        return self.progressConversation
+    
+    def getCompletedConversation(self):
+        if not self.completedConversation:
+            node = C.ConversationNode("Thank you! That is one Mystical MacGuffin!")
+            node.createOption("No problem")
+            self.completedConversation = C.ConversationTree([node])
+        return self.completedConversation
 
 
         
